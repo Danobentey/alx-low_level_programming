c390e00e90bfca50d0197279b9b6f56a7695f191
Daniel <danobentey@gmail.com>
2023-09-13 22:11:19 -0700

    debug

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,24 +1,16 @@
-#include <unistd.h>
+#include "main.h"
 
-void _putchar(char c) {
-    write(1, &c, 1);
+/**
+ * add - adds two integers and returns the result
+ * @a: int to be added to b
+ * @b: int to be added to a
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
 }
-
-void print_alphabet_x10(void) {
-    char letter = 'a';
-
-    for (int i = 0; i < 10; i++) {
-        while (letter <= 'z') {
-            _putchar(letter);
-            letter++;
-        }
-        _putchar('\n');
-        letter = 'a'; // Reset the letter to 'a' for the next line
-    }
-}
-
-int main() {
-    print_alphabet_x10();
-    return (0);
-}
-
