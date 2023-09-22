d7a676a87e9f02a429436140630e85b54aedd5f4
Daniel <danobentey@gmail.com>
2023-09-22 06:40:29 -0700

    fix header misspelling

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,28 +1,28 @@
 #include "main.h"
 
 /**
- * leet - Encodes a string into 1337.
- * @str: The string to encode.
+ * leet - encodes a string in 1337
+ * @s: string to be encoded
  *
- * Return: A pointer to the resulting encoded string.
+ * Return: the resulting string;
  */
-char *leet(char *str)
+char *leet(char *s)
 {
-    char leet_map[128] = {0}; // Initialize an array to map characters
+	int i, j;
 
-    // Define mapping for lowercase characters
-    leet_map['a'] = leet_map['A'] = '4';
-    leet_map['e'] = leet_map['E'] = '3';
-    leet_map['o'] = leet_map['O'] = '0';
-    leet_map['t'] = leet_map['T'] = '7';
-    leet_map['l'] = leet_map['L'] = '1';
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
 
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (leet_map[str[i]] != 0) {
-            str[i] = leet_map[str[i]];
-        }
-    }
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+			}
+		}
+	}
 
-    return str;
+	return (s);
 }
-
